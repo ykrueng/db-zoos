76bd4b3e2e6a6d4bf476a2a2d5f63dd4ba164434
ykrueng <yasirah.krueng@gmail.com>
2019-01-15 16:39:39 -0600

    fix checkName middleware to check for duplicate

--- api/common/checkName.js
+++ api/common/checkName.js
@@ -1,9 +1,17 @@
-module.exports = (req, res, next) => {
+const zoosDb = require("../zoos/zoosDb");
+
+module.exports = async (req, res, next) => {
   const { name } = req.body;
 
   if (!name || typeof name !== "string" || name.length > 128) {
     next({ code: 400 });
-  }
+  } else {
+    const zoos = await zoosDb.get();
 
-  next();
+    if (zoos.find(zoo => zoo.name === name)) {
+      next({ code: 400 });
+    } else {
+      next();
+    }
+  }
 };
